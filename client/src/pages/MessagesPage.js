d7d71a261f095a8bc7852a1f943ef3ce21c7ad61
Josh-Ay <ayo.oloyede16@gmail.com>
2022-03-15 23:25:04 +0100

    Update styles

--- client/src/pages/MessagesPage.js
+++ client/src/pages/MessagesPage.js
@@ -52,7 +52,7 @@ const MessagesPage = ( { user, socketInstance } ) => {
 
     useTitle("Messages | Tweeter");
     useChangeElementPropertyOnScroll(mobileNavRef, "display", "flex", "none");
-    useChangeElementPropertyOnScroll(createMessageContainerRef, "bottom", isSmallScreen ? isVerySmallScreen ? "9%" : "11%" : "0", "0");
+    useChangeElementPropertyOnScroll(createMessageContainerRef, "bottom", isSmallScreen ? isVerySmallScreen ? "7.7%" : "11%" : "0", "0");
     useClickOutside(ref, () => setActive(false));
     
     // handle input change
