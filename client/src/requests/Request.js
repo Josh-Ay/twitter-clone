88c0aeac4cb279cacc5a2147dd40e6daee28f3a5
Josh-Ay <ayo.oloyede16@gmail.com>
2022-03-15 02:26:33 +0100

    Comment blocks of code in the client requests file

--- client/src/requests/Request.js
+++ client/src/requests/Request.js
@@ -8,14 +8,17 @@ axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 class Request{
 
+    // make post request to a route on the server passing the data you would like to post
     static makePostRequest = async (routeToPostTo, dataToPost) => {
         return await axios.post(routeToPostTo, qs.stringify(dataToPost));
     }
     
+    // make a get request to a route on the server
     static makeGetRequest = async (route) => {
         return await axios.get(route);
     }
 
+    // make a get request with credentials to a route on the server
     static makeGetRequestWithCredentials = async (route) => {
         const request = await axios.get(route, {
             withCredentials: true
@@ -23,10 +26,12 @@ class Request{
         return request;
     }
     
+    // make a patch request to a route on the server
     static makePatchRequest = async (route, dataToPut) => {
         return await axios.patch(route, qs.stringify(dataToPut));
     }
     
+    // upload a single image to a route on the server
     static uploadImage = async (routeToPostTo, imageFileToUpload) => {
         const request = await axios.patch(routeToPostTo, imageFileToUpload, {
             headers: {
@@ -36,6 +41,7 @@ class Request{
         return request;
     }
     
+    // make a multi-part post request(request with images if any) to a route on the server
     static makeMultipartPostRequest = async (routeToPostTo, dataToPost) => {
         const request = await axios.post(routeToPostTo, dataToPost, {
             headers: {
