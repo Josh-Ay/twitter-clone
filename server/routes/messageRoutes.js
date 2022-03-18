ae6709937edf7d6a49cbae5897ebb5e34a5bbf76
Josh-Ay <ayo.oloyede16@gmail.com>
2022-03-18 23:54:32 +0100

    Add route for marking messages as read

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -9,5 +9,6 @@ const router = express.Router();
 // message routes
 router.get("/messages/:userId", messagesController.get_user_messages);
 router.post("/messages/:senderUserId/:receiverUserId", multerUpload.single("messageImage"), messagesController.post_new_message);
+router.post("/messages/:senderUserId/:receiverUserId/update_read", messagesController.mark_all_messages_read);
 
 module.exports = router;
